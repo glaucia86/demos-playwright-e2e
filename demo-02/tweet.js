4a161fee229bd5a8ac5bbf0622cceca0c060eb02
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2021-04-30 22:25:44 -0300

    Finalizando a demo-02 - descobrir como realizar tweet com Playwright

--- demo-02/tweet.js
+++ demo-02/tweet.js
@@ -7,11 +7,20 @@
  * Twitter: <@glaucia_lemos86>
  */
 
-const { chromium } = require('playwright');
+const { chromium, webkit, devices } = require('playwright');
 
 (async () => {
-    const browser = await chromium.launch(); 
-    const page = await browser.newPage();
+  const browser = await chromium.launch({ headless: false });
+  const page = await browser.newPage();
 
-    await page.goto('https://twitter.com.login');
-})
+  await page.goto('https://twitter.com/login');
+  await page.fill('input[type="text"]', 'glaucia_lemos86');
+  await page.fill('input[type="password"]', 'Glau32759');
+  await page.click('div[data-testid="LoginForm_Login_Button"]')
+
+  // Agora vamos fazer um tweet automatizado usando o Playwright:
+
+  await page.fill('.public-DraftStyleDefault-ltr', 'Realizando um tweet automatizado, utilizando a ferramenta de Testes do Playwright - Demo em breve e tutorial sobre o assunto! 😎 (Link: https://playwright.dev/)');
+  await page.click('div[data-testid="tweetButtonInline"]');
+
+})();
