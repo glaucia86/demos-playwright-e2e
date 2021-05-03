5b2be1b535e73260cda807725701bdc71e86a6c0
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2021-05-03 07:09:23 -0300

    Finalizando a demo-02

--- demo-02/tweet.js
+++ demo-02/tweet.js
@@ -10,19 +10,26 @@
 const { chromium, webkit, devices } = require('playwright');
 
 (async () => {
-    const browser = await chromium.launch({ headless: false });
-    const page = await browser.newPage();
+  const browser = await chromium.launch({ headless: false });
+  // const page = await browser.newPage();
 
-    await page.goto('https://twitter.com/login');
-    await page.fill('input[type="text"]', '<include-twitter-handler>');
-    await page.fill('input[type="password"]', '<include-password>');
-    await page.click('div[data-testid="LoginForm_Login_Button"]')
+  const context = await browser.newContext({
+    recordVideo: {
+      dir: "./video-recording"
+    }
+  });
+  const page = await context.newPage();
 
-    // Agora vamos fazer um tweet automatizado usando o Playwright:
+  await page.goto('https://twitter.com/login');
+  await page.fill('input[type="text"]', '<include-your-twitter-handler-here>');
+  await page.fill('input[type="password"]', '<include-your-twitter-password-here>');
+  await page.click('div[data-testid="LoginForm_Login_Button"]')
 
-    await page.click('.public-DraftStyleDefault-ltr');
-    await page.keyboard.type('Realizando um tweet automatizado, utilizando a ferramenta de Testes do Playwright - Demo em breve e tutorial sobre o assunto! 😎 (Link: https://playwright.dev/)');
-    await page.click('div[data-testid="tweetButtonInline"]');
+  // Agora vamos fazer um tweet automatizado usando o Playwright:
+
+  await page.click('.public-DraftStyleDefault-ltr');
+  await page.keyboard.type('Realizando um tweet automatizado, utilizando a ferramenta de Testes do Playwright - Demo em breve e tutorial sobre o assunto! 😎 (Link: https://playwright.dev/)');
+  await page.click('div[data-testid="tweetButtonInline"]');
 
 })();
 
