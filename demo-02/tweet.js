ecc86026e1becef261eae8f796868f80d324121b
glaucia86 <glaucia_lemos86@hotmail.com>
2021-06-08 18:13:33 -0300

    Alterando o tamanho da resolução da gravação dos vídeos automatizados pelo playwright

--- demo-02/tweet.js
+++ demo-02/tweet.js
@@ -1,4 +1,3 @@
-
 /**
  * Arquivo: demo-02/tweet.js
  * Data: 01/05/2021
@@ -7,28 +6,32 @@
  * Twitter: <@glaucia_lemos86>
  */
 
-const { chromium, webkit, devices } = require('playwright');
+const { chromium, webkit, devices } = require("playwright");
 
 (async () => {
   const browser = await chromium.launch({ headless: false });
 
   const context = await browser.newContext({
     recordVideo: {
-      dir: "./video-recording"
-    }
+      dir: "./video-recording",
+      size: {
+        width: 1024,
+        height: 786,
+      },
+    },
   });
   const page = await context.newPage();
 
-  await page.goto('https://twitter.com/login');
-  await page.fill('input[type="text"]', '<include-your-twitter-handler-here>');
-  await page.fill('input[type="password"]', '<include-your-twitter-password-here>');
-  await page.click('div[data-testid="LoginForm_Login_Button"]')
+  await page.goto("https://twitter.com/login");
+  await page.fill('input[type="text"]', "<include-your-twitter-handler>");
+  await page.fill('input[type="password"]', "<include-your-password>");
+  await page.click('div[data-testid="LoginForm_Login_Button"]');
 
   // Agora vamos fazer um tweet automatizado usando o Playwright:
 
-  await page.click('.public-DraftStyleDefault-ltr');
-  await page.keyboard.type('Realizando um tweet automatizado, utilizando a ferramenta de Testes do Playwright - Demo em breve e tutorial sobre o assunto! 😎 (Link: https://playwright.dev/)');
+  await page.click(".public-DraftStyleDefault-ltr");
+  await page.keyboard.type(
+    "Fala Coders! Tweet Automatizado com @playwrightweb & Node.js. Link da demo: https://github.com/glaucia86/demos-playwright-e2e"
+  );
   await page.click('div[data-testid="tweetButtonInline"]');
-
 })();
-
