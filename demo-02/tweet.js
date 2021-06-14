d4342f758e37a0f621e7f5c90d76e310b23d1e00
glaucia86 <glaucia_lemos86@hotmail.com>
2021-06-14 13:45:35 -0300

    Alterando as configurações de video da demo-02

--- demo-02/tweet.js
+++ demo-02/tweet.js
@@ -1,6 +1,6 @@
 /**
  * Arquivo: demo-02/tweet.js
- * Data: 01/05/2021
+ * Data: 14/06/2021
  * Descrição: arquivo responsável por realizar a demo automatizando tweets usando o Playwright
  * Autora: Glaucia Lemos
  * Twitter: <@glaucia_lemos86>
@@ -12,6 +12,10 @@ const { chromium, webkit, devices } = require("playwright");
   const browser = await chromium.launch({ headless: false });
 
   const context = await browser.newContext({
+    viewport: {
+      width: 1920,
+      height: 1080,
+    },
     recordVideo: {
       dir: "./video-recording",
       size: {
@@ -20,18 +24,19 @@ const { chromium, webkit, devices } = require("playwright");
       },
     },
   });
+
   const page = await context.newPage();
 
   await page.goto("https://twitter.com/login");
-  await page.fill('input[type="text"]', "");
-  await page.fill('input[type="password"]', "");
+  await page.fill('input[type="text"]', "<include-your-twitter-handle-here>");
+  await page.fill('input[type="password"]', "<include-your-password-here>");
   await page.click('div[data-testid="LoginForm_Login_Button"]');
 
   // Agora vamos fazer um tweet automatizado usando o Playwright:
 
   await page.click(".public-DraftStyleDefault-ltr");
   await page.keyboard.type(
-    "Fala Coders! Tweet Automatizado com @playwrightweb & Node.js. Link da demo: https://github.com/glaucia86/demos-playwright-e2e"
+    "Tweet automatizado utilizando a ferramenta de Testes do Playwright - Mais informações: https://github.com/glaucia86/demos-playwright-e2e 😎"
   );
   await page.click('div[data-testid="tweetButtonInline"]');
 })();
